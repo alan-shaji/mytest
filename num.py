23a53c23100c76d48d46adb2507a36079cc28a63
alan-shaji <allen0598@gmail.com>
2019-04-26 16:44:01 +0530

    edit in readme

---
+++ num.py
@@ -0,0 +1,8 @@
+import sys 
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        words = line.split()
+        for i in words:
+            for letter in i:
+                if(letter.isdigit()):
+                    print(letter)
